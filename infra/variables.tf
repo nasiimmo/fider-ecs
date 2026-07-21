cf20b337de652173bb319729cbc6a9fdd9564d3e
Nasiim Nuur <nasiimmo.691@gmail.com>
2026-07-21 04:22:41 +0100

    fix: update db_username default and add to pipeline env

--- infra/variables.tf
+++ infra/variables.tf
@@ -49,7 +49,7 @@ variable "db_name" {
 variable "db_username" {
   description = "Master username for the RDS instance"
   type        = string
-  default     = "admin"
+  default     = "fider"
 }
 
 
