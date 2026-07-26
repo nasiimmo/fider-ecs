cbf51aca26ae2ab184b71c4c842dd2bf9d2be4fc
Nasiim Nuur <nasiimmo.691@gmail.com>
2026-07-26 12:41:43 +0100

    feat: add ecr lifecycle policy to keep last 5 images [skip ci]

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -12,3 +12,24 @@ resource "aws_ecr_repository" "main" {
     environment = var.environment
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "main" {
+  repository = aws_ecr_repository.main.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 5 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 5
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
